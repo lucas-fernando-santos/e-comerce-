c63f3b445992b3e4ea406262fd3ef5c3c9cca14f
Lucas Fernando <lfmanofl@gmail.com>
2022-12-23 15:58:05 -0300

    meu primeiro commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+    <header>
+        <div className="perfil">
+            im
+        </div>
+        <div className="infoPerfil"></div>
+        <div className="notification"></div>
+    </header>
+    </>
+  )
+}
+
+export default index
