cb7ae46aa562bfba9154ebe61ff2f2f2b86ce5d6
Lucas Fernando <lfmanofl@gmail.com>
2022-12-26 12:02:16 -0300

    finalizada barra de categorias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import CategoriesCarrosel from './components/CategoriesCarrosel'
 import Home from './pages/home'
 
 const App = () => {
   return (
     <>
     <Home/>
+    <CategoriesCarrosel/>
     </>
   )
 }
---
+++ src/components/CategoriesCarrosel/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import './style.css'
+const categorias = [
+    {
+        id: 0,
+        name: 'Todos'
+    },
+    {
+        id: 1,
+        name: 'Nike'
+    },
+    {
+        id: 2,
+        name: 'Puma'
+    },
+    {
+        id: 3,
+        name: 'Adidas'
+    },
+    {
+        id: 4,
+        name: 'Adidas'
+    },
+    {
+        id: 5,
+        name: 'Tesla'
+    },
+    {
+        id: 6,
+        name: 'LV'
+    },
+    {
+        id: 7,
+        name: 'New Balance'
+    },
+]
+function CategoriesCarrosel() {
+    const [selected, setSelected] = useState(0)
+
+    return (
+        <div>
+            <div className="marcas">
+                <div className="textMarcas">
+                    <h2>categorias</h2>
+                </div>
+                <div className="containerElements">
+                    {categorias.map(item => (
+                        <button
+                            key={item.id}
+                            onClick={() => setSelected(item.id)}
+                            style={{ backgroundColor: selected == item.id ? 'black' : 'white', color: selected == item.id ? 'white' : 'black' }}
+                        >{item.name}</button>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CategoriesCarrosel
